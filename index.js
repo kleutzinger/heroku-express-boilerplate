363401290bffaa94cd75437aba2eb274fd2666bd
Kevin Leutzinger <me@kevinleutzinger.com>
2020-09-02 12:53:11 -0700

    packages

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 // api code
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
-
-
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 //
 
 app.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`);
-})
+  console.log(`server listening on port ${PORT}`);
+});
