36457c63df80a8e41e64581d9a22a08af095324e
Kevin Leutzinger <me@kevinleutzinger.com>
2020-09-03 21:45:23 -0700

    hpush

--- db_setup.js
+++ db_setup.js
@@ -1,9 +1,12 @@
 const { Pool } = require('pg');
 var config = {
-  connectionString : process.env.DATABASE_URL,
-  ssl              : {
+  connectionString        : process.env.DATABASE_URL,
+  ssl                     : {
     rejectUnauthorized : false
-  }
+  },
+  max                     : 5,
+  idleTimeoutMillis       : 10000,
+  connectionTimeoutMillis : 2000
 };
 var pool = new Pool(config);
 
