5783d0f0f89d98b5e7206db742b9e6afa0b004c0
Kevin Leutzinger <me@kevinleutzinger.com>
2020-09-04 00:26:21 -0700

    hpush

--- api.js
+++ api.js
@@ -49,7 +49,7 @@ async function new_upload(dl_url, metadata = {}) {
     const text = 'INSERT INTO upload_history(dl_url, filename) VALUES($1, $2);';
     const values = [ dl_url, filename ];
     const resp = await client.query(text, values);
-    await client.release();
+    client.release();
     return resp;
   } catch (err) {
     client.release();
@@ -64,7 +64,7 @@ async function new_metadata(filename, metadata = {}) {
     const text = 'INSERT INTO slippi_meta(filename, metadata) VALUES($1, $2);';
     const values = [ filename, metadata ];
     const resp = await client.query(text, values);
-    await client.release();
+    client.release();
     return resp;
   } catch (err) {
     client.release();
@@ -103,7 +103,7 @@ async function get_upload_history() {
     const client = await pool.connect();
     const text = `SELECT * from upload_history INNER JOIN slippi_meta on upload_history.filename=slippi_meta.filename ORDER BY created_at desc;`;
     resp = await client.query(text);
-    await client.release();
+    client.release();
     return resp;
   } catch (err) {
     client.release();
