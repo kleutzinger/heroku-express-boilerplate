873d61a1a8bca32da7265d47346180a9b1b18947
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-04-12 11:08:15 -0700

    add auth0

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.use(bodyParser.json());
 app.set('view engine', 'pug');
 // app.use(multer({ dest: './tmp' }).any());
 
+const { auth, requiresAuth } = require('express-openid-connect');
+
+const auth0_config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env['AUTH0_RANDOM_STR'],
+  baseURL: 'http://localhost:5000',
+  clientID: '3XvPWaMAd0jgDEMJOKHe4lUbcH6Tt6iK',
+  issuerBaseURL: 'https://dev-4gfidufu1t4at7rq.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(auth0_config));
+
+
 app.use(require('serve-favicon')(path.join('web', 'monocle.ico')));
 app.use(express.static('web'));
 if (!fs.existsSync('slp')) {
@@ -31,7 +46,7 @@ const server = app.listen(PORT, () => {
 const io = socketIO(server);
 
 const uploadRouter = require('./upload_ingester.js')(io);
-app.use('/upload', uploadRouter); // Forwards any requests to the /albums URI to our albums Router
+app.use('/upload', requiresAuth(), uploadRouter); // Forwards any requests to the /albums URI to our albums Router
 
 const { apiRouter, get_upload_history } = require('./api.js');
 const { squid } = require('./squid_setup.js');
@@ -40,7 +55,7 @@ app.use('/api', apiRouter);
 app.get('/', async function (req, res, next) {
   try {
     const rows = await get_upload_history(squid);
-    res.render('home', { upload_history: JSON.stringify(rows) });
+    res.render('home', { upload_history: JSON.stringify(rows), is_logged_in: req.oidc.isAuthenticated()});
   } catch (error) {
     next(error);
   }
@@ -52,4 +67,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     //console.log('client disc')
   });
+});
+
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
